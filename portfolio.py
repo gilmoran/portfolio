c370fc6948e44a21f1bb051abdca179e6a3486d2
gilmoran <93230827+gilmoran@users.noreply.github.com>
2021-11-16 21:06:14 -0500

    Update portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -3,7 +3,7 @@ class Portfolio:
         self._stocks = []
 
     def buy(self, name, shares, price):
-        self._stocks.append((name , shares , price))  
+        self._stocks.append((name ,shares ,price))  
 
     def cost(self):
         return sum(
