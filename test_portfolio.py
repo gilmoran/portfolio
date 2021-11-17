d7c673df67521143bd35b06a195f5806cd4f70f4
gilmoran <93230827+gilmoran@users.noreply.github.com>
2021-11-16 21:16:10 -0500

    Update test_portfolio.py

--- test_portfolio.py
+++ test_portfolio.py
@@ -10,3 +10,7 @@ def test_buy_two_stocks():
     p.buy("IBM", 100, 176.48)
     p.buy("HPQ", 100, 36.15)
     assert p.cost() == 21263.00
+def test_buy_no_stocks():
+    p = Portfolio()
+    p.buy("IBM", 0, 0)
+    assert p.cost() == 0
