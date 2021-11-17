194fb48aa2181c8704cf8f7cdca9d876a119c742
gilmoran <93230827+gilmoran@users.noreply.github.com>
2021-11-16 20:15:59 -0500

    implement stock portfolio

---
+++ portfolio.py
@@ -0,0 +1,15 @@
+class Portfolio: 
+    def __init__(self):
+        self._stocks = []
+        
+    def buy(self, name, shares, price):
+        self._stocks.append(name,shares,price)  
+
+    def cost(self):
+        return sum(
+            shares * price for name, shares, price in self._stocks 
+        )
+       
+
+
+    
